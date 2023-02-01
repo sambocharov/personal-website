a7442936706d35f011e22bf87c327167e395b4e9
Sam Bocharov <semen_bard@mail.ru>
2023-02-01 19:04:21 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      Sam Bocharov's personal website
+      <p>Coming soon</p>
+    </div>
+  )
+}
+
+export default App
