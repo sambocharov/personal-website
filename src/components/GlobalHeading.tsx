e39e74a399b7f7150d161aed3e4131848344536c
Sam Bocharov <dev.sambocharov@gmail.com>
2023-12-22 23:15:50 +0000

    shame message

--- src/components/GlobalHeading.tsx
+++ src/components/GlobalHeading.tsx
@@ -4,7 +4,8 @@ import './GlobalHeading.scss';
 const GlobalHeading = () => {
   return (
     <header>
-      Sam Bocharov. Developer.
+      <div>Sam Bocharov. Developer. </div>
+      <div className="shame-message">I know website is not responsive...yet. I'm working on it. Yes, I'm ashamed</div>
     </header>
   )
 }
