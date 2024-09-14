746440740b3247cfcaa1bffbc9e812c51aaa1228
Sam Bocharov <dev.sambocharov@gmail.com>
2024-09-14 19:45:04 +0100

    new styling

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,14 +1,18 @@
 import { translate } from '../utils/translation/translate';
 import './Navigation.scss';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import classNames from 'classnames';
 
 const Navigation = () => {
+  const { hash, pathname, search } = useLocation();
+  console.log(pathname);
+  
   return (
     <div className="navigation">
-      <Link to="/" className="nav-element"><span>{translate('about_menu')}</span></Link>
-      <Link to="/cv" className="nav-element"><span>{translate('cv')}</span></Link>
-      <Link to="/tools" className="nav-element"><span>{translate('tools')}</span></Link>
-      <Link to="/blog" className="nav-element"><span>{translate('blog')}</span></Link>
+      <Link to="/" className={classNames("nav-element", { 'highlighted': pathname === '/'})}><span>{translate('about_menu')}</span></Link>
+      <Link to="/cv" className={classNames("nav-element", { 'highlighted': pathname === '/cv'})}><span>{translate('cv')}</span></Link>
+      <Link to="/tools" className={classNames("nav-element", { 'highlighted': pathname === '/tools'})}><span>{translate('tools')}</span></Link>
+      <Link to="/blog" className={classNames("nav-element", { 'highlighted': pathname === '/blog'})}><span>{translate('blog')}</span></Link>
     </div>
   )
 }
